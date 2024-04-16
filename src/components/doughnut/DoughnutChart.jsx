db0e0eecec0620756f6d1073ec498a727e5b622a
edthuman <edwardadirvine@aol.com>
2024-04-16 17:22:56 +0100

    add error handling to DoughnutChart and PolarAreaChart

--- src/components/doughnut/DoughnutChart.jsx
+++ src/components/doughnut/DoughnutChart.jsx
@@ -48,7 +48,13 @@ function DoughnutChart({petitionData, selectedScope}) {
         signaturesPerLabel.push(allScopeData[i].signature_count)
         } else ukSignatureCount += allScopeData[i].signature_count
     }
-    return <Doughnut data={data} options={options}/>
+    return <>
+        {allScopeData.length ? (<>
+        {selectedScope === "world" ? <p>The United Kingdom has been excluded from the data, it had {ukSignatureCount} signatures</p> : null}
+        <p>Highlight over the bars to see which {selectedScope === "world" ? "country" : (selectedScope === "uk-regions" ? "region" : "constituency")} they are for</p>
+        <Doughnut data={data} options={options}/>
+        </>) : <p>Signature counts by location are unavailable for this petition, it had {petitionData.signatureCount} signatures in total</p>}
+        </>
 }
 
 export default DoughnutChart
